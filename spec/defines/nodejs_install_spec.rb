9300d3fb56bf707fff7111c11bb5eeae4d2f1c79
Johannes Graf <graf@synyx.de>
2013-10-15 00:35:50 +0200

    added real support for multiple installed versions of nodejs
    It was not possible to install multiple different versions of nodejs
    e.g.:
    nodejs::install{"v0.10.20": version => "v0.10.20", make_install => false}
    nodejs::install{"v0.10.19": version => "v0.10.19", make_install => false}
    
    * fixed double resource definition errors
    * an archive is extracted into /usr/local/node/node-$version
    * nodejs::install symlink of nodejs contains the version
    * class nodejs is responsible for symlink $target_dir/node
    * enhanced ./configure to install nodejs into /usr/local/node/node-
    $version instead of /usr/local
    * fixed puppet lint issues
    * fixed node symlink issue with non semver versions
    * improved default values handling
    * refactored nodejs class spec
    * fixed typo inside README.md
    * fixed broken spec for define nodejs::install
    * enhanced spec for define nodejs::install
    
    Known limitation: just a single nodejs::install define of versions below
    v0.6.3 is supportet!

--- spec/defines/nodejs_install_spec.rb
+++ spec/defines/nodejs_install_spec.rb
@@ -6,40 +6,129 @@ describe 'nodejs::install', :type => :define do
     :nodejs_stable_version => 'v0.10.20'
   }}
 
-  it { should contain_file('nodejs-install-dir-v0.10.20').with_ensure('directory') }
+  describe 'with default parameters' do
+    
+    let(:params) {{ }}
 
-  it { should contain_wget__fetch('nodejs-download-v0.10.20') \
-    .with_source('http://nodejs.org/dist/v0.10.20/node-v0.10.20.tar.gz') \
-    .with_destination('/usr/local/node/node-v0.10.20.tar.gz')
-  }
+    it { should contain_file('nodejs-install-dir') \
+      .with_ensure('directory')
+    }
 
-  it { should contain_file('nodejs-check-tar-v0.10.20') \
-    .with_ensure('file') \
-    .with_path('/usr/local/node/node-v0.10.20.tar.gz')
-  }
+    it { should contain_wget__fetch('nodejs-download-v0.10.20') \
+      .with_source('http://nodejs.org/dist/v0.10.20/node-v0.10.20.tar.gz') \
+      .with_destination('/usr/local/node/node-v0.10.20.tar.gz')
+    }
 
-  it { should contain_exec('nodejs-unpack-v0.10.20') \
-    .with_command('tar xzvf node-v0.10.20.tar.gz') \
-    .with_cwd('/usr/local/node') \
-    .with_unless('test -d /usr/local/node/node-v0.10.20')
-  }
+    it { should contain_file('nodejs-check-tar-v0.10.20') \
+      .with_ensure('file') \
+      .with_path('/usr/local/node/node-v0.10.20.tar.gz')
+    }
 
-  it { should contain_file('nodejs-check-unpack-v0.10.20') \
-    .with_ensure('directory') \
-    .with_path('/usr/local/node/node-v0.10.20')
-  }
+    it { should contain_exec('nodejs-unpack-v0.10.20') \
+      .with_command('tar -xzvf node-v0.10.20.tar.gz -C /usr/local/node/node-v0.10.20 --strip-components=1') \
+      .with_cwd('/usr/local/node') \
+      .with_unless('test -f /usr/local/node/node-v0.10.20/bin/node')
+    }
 
-  it { should contain_exec('nodejs-make-install-v0.10.20') \
-    .with_cwd('/usr/local/node/node-v0.10.20') \
-    .with_unless('test -f /usr/local/node/node-v0.10.20/node')
-  }
+    it { should contain_file('/usr/local/node/node-v0.10.20') \
+      .with_ensure('directory')
+    }
 
-  it { should contain_file('nodejs-symlink-bin-v0.10.20') \
-    .with_ensure('link') \
-    .with_path('/usr/local/bin/node') \
-    .with_target('/usr/local/node/node-v0.10.20/node')
-  }
+    it { should contain_exec('nodejs-make-install-v0.10.20') \
+      .with_command('./configure --prefix=/usr/local/node/node-v0.10.20 && make && make install') \
+      .with_cwd('/usr/local/node/node-v0.10.20') \
+      .with_unless('test -f /usr/local/node/node-v0.10.20/bin/node')
+    }
 
-  it { should_not contain_wget__fetch('npm-download-v0.10.20') }
-  it { should_not contain_exec('npm-install-v0.10.20') }
+    it { should contain_file('nodejs-symlink-bin-with-version-v0.10.20') \
+      .with_ensure('link') \
+      .with_path('/usr/local/bin/node-v0.10.20') \
+      .with_target('/usr/local/node/node-v0.10.20/bin/node')
+    }
+
+    it { should_not contain_file('/usr/local/bin/node') }
+    it { should_not contain_file('/usr/local/bin/npm') }
+
+    it { should_not contain_wget__fetch('npm-download-v0.10.20') }
+    it { should_not contain_exec('npm-install-v0.10.20') }
+  end
+
+  describe 'with specific version v0.10.19' do
+
+    let(:params) {{
+      :version => 'v0.10.19'
+    }}
+
+    it { should contain_file('nodejs-install-dir') \
+      .with_ensure('directory')
+    }
+
+    it { should contain_wget__fetch('nodejs-download-v0.10.19') \
+      .with_source('http://nodejs.org/dist/v0.10.19/node-v0.10.19.tar.gz') \
+      .with_destination('/usr/local/node/node-v0.10.19.tar.gz')
+    }
+
+    it { should contain_file('nodejs-check-tar-v0.10.19') \
+      .with_ensure('file') \
+      .with_path('/usr/local/node/node-v0.10.19.tar.gz')
+    }
+
+    it { should contain_exec('nodejs-unpack-v0.10.19') \
+      .with_command('tar -xzvf node-v0.10.19.tar.gz -C /usr/local/node/node-v0.10.19 --strip-components=1') \
+      .with_cwd('/usr/local/node') \
+      .with_unless('test -f /usr/local/node/node-v0.10.19/bin/node')
+    }
+
+    it { should contain_file('/usr/local/node/node-v0.10.19') \
+      .with_ensure('directory')
+    }
+
+    it { should contain_exec('nodejs-make-install-v0.10.19') \
+      .with_command('./configure --prefix=/usr/local/node/node-v0.10.19 && make && make install') \
+      .with_cwd('/usr/local/node/node-v0.10.19') \
+      .with_unless('test -f /usr/local/node/node-v0.10.19/bin/node')
+    }
+
+    it { should contain_file('nodejs-symlink-bin-with-version-v0.10.19') \
+      .with_ensure('link') \
+      .with_path('/usr/local/bin/node-v0.10.19') \
+      .with_target('/usr/local/node/node-v0.10.19/bin/node')
+    }
+
+    it { should_not contain_file('/usr/local/bin/node') }
+    it { should_not contain_file('/usr/local/bin/npm') }
+
+    it { should_not contain_wget__fetch('npm-download-v0.10.19') }
+    it { should_not contain_exec('npm-install-v0.10.19') }
+  end
+
+
+  describe 'with a given target_dir' do
+    let(:params) {{
+      :target_dir => '/bin'
+    }}
+
+    it { should contain_file('nodejs-symlink-bin-with-version-v0.10.20') \
+      .with_ensure('link') \
+      .with_path('/bin/node-v0.10.20') \
+      .with_target('/usr/local/node/node-v0.10.20/bin/node')
+    }
+  end
+
+  describe 'without NPM' do
+    let(:params) {{
+      :with_npm => false
+    }}
+
+    it { should_not contain_exec('npm-download-v0.10.20') }
+    it { should_not contain_exec('npm-install-v0.10.20') }
+  end
+
+  describe 'with make_install = false' do
+    let(:params) {{
+      :make_install => false
+    }}
+
+    it { should_not contain_exec('nodejs-make-install-v0.10.20') }
+  end
 end
