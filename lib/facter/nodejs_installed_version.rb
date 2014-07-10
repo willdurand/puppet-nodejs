e860b624ef89809d08c02057856fbd788ac6e4fb
gpkfr <github@imelbox.com>
2014-07-11 00:34:04 +0200

    Update nodes_installed_version
    you must use full path to permit $::nodes_installed_version work as expected ;)

--- lib/facter/nodejs_installed_version.rb
+++ lib/facter/nodejs_installed_version.rb
@@ -1,5 +1,5 @@
 Facter.add("nodejs_installed_version") do
   setcode do
-    Facter::Util::Resolution.exec('node -v 2> /dev/null')
+    Facter::Util::Resolution.exec('/usr/local/node/node-default/bin/node -v 2> /dev/null')
   end
 end
