ef2d8fef429a3a0e76dee3220d9aa574780de8f0
Maximilian Bosch <maximilian.bosch.27@gmail.com>
2016-01-17 17:48:57 +0100

    #94 - create version-aware executables to provide multiple npm installations

---
+++ lib/puppet/parser/functions/npm_version.rb
@@ -0,0 +1,11 @@
+module Puppet::Parser::Functions
+  newfunction(:npm_version, :type => :rvalue) do |args|
+    node_unpack_folder = args[0]
+    version            = `#{node_unpack_folder}/bin/npm -v`
+
+    # the version will be extracted from the CLI.
+    # the output contains a line break, but this line break causes weird
+    # symlink names, so the version needs to be stripped.
+    version.strip
+  end
+end
