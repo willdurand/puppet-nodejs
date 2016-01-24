1eea4119c6a67102250278f73ef780e8d5dafba7
Maximilian Bosch <maximilian.bosch.27@gmail.com>
2016-01-24 01:54:12 +0100

    #123 - #40 - minor review cleanup

--- lib/puppet/parser/functions/node_default_instance_directory.rb
+++ lib/puppet/parser/functions/node_default_instance_directory.rb
@@ -3,7 +3,7 @@ module Puppet::Parser::Functions
     target_dir  = args[0]
     default_dir = "#{target_dir}/node-default"
 
-    unless is_dir(default_dir)
+    unless File.exist?(default_dir)
       raise Puppet::Error, "Invalid node symlink #{default_dir}! " \
         "(Did you try to remove a nodejs instance " \
         "a run where the node setup will be done the first time?)"
@@ -11,12 +11,4 @@ module Puppet::Parser::Functions
 
     File.readlink(default_dir)
   end
-end
-
-def read_link(dir)
-  File.readlink(dir)
-end
-
-def is_dir(dir)
-  File.exist?(dir)
 end
