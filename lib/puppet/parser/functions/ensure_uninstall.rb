d8e4fce13646b4ff771f4cfa27b9ff94eb44f082
Maximilian Bosch <maximilian.bosch.27@gmail.com>
2016-10-16 17:00:51 +0200

    revert minor behavior change
    allow to compute a version to be removed (instead of fully quallified
    versions a string like `latest`).

--- lib/puppet/parser/functions/ensure_uninstall.rb
+++ lib/puppet/parser/functions/ensure_uninstall.rb
@@ -2,8 +2,16 @@ module Puppet::Parser::Functions
   newfunction(:ensure_uninstall, :type => :rvalue) do |args|
     raise(Puppet::ParseError, 'ensure_uninstall(): too few arguments!') if args.size < 1
 
-    args[0].map {
-      |version| ["nodejs-uninstall-custom-#{version}", { :version => version }]
-    }.to_h
+    instances  = args[0]
+    normalized = instances.map do |version|
+      Puppet::Parser::Functions.function(:evaluate_version)
+      actual_version = function_evaluate_version([version])
+      [
+        "nodejs-uninstall-custom-#{actual_version}",
+        { :version => function_evaluate_version([version]) }
+      ]
+    end
+
+    normalized.to_h
   end
 end
