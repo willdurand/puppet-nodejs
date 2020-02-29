b22165a6eab8e3a3be0d9cf6cdcc962821aa4141
Maximilian Bosch <maximilian@mbosch.me>
2020-02-29 01:28:56 +0100

    Automatigally migrate native functions written in Ruby to new structure
    Closes #194
    Co-authored-by: Ben Ford <ben.ford@puppetlabs.com>

--- lib/puppet/parser/functions/source_filename.rb
+++
@@ -1,7 +0,0 @@
-module Puppet::Parser::Functions
-  newfunction(:source_filename, :type => :rvalue) do |args|
-    raise(Puppet::ParseError, 'source_filename(): too few arguments') if args.empty?
-
-    args[0].rpartition('/').last
-  end
-end
